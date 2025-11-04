0d8dec52e8b23d6b89733259e34465524a96ca4b
이동욱 <151100376+Hiroo4763@users.noreply.github.com>
2025-11-04 11:45:56 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251104/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    for(int i = 0; i < 5; i++){
+        for(int i = 0; i < 10; i++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
